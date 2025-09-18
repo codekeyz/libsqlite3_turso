55efa02b6c49b2b2bdf26447bea1ebedddf7b992
Chima Precious <ositachima.co@gmail.com>
2025-09-18 12:51:57 +0000

    _

---
+++ lib/database.dart
@@ -0,0 +1,24 @@
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:sqlite3/sqlite3.dart';
+
+part 'database.g.dart';
+
+class TodoItems extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text().withLength(min: 6, max: 32)();
+  TextColumn get content => text().named('body')();
+  DateTimeColumn get createdAt => dateTime().nullable()();
+}
+
+@DriftDatabase(tables: [TodoItems])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+
+  static QueryExecutor _openConnection() {
+    return NativeDatabase.opened(sqlite3.open('simple-nubian.db'));
+  }
+}
